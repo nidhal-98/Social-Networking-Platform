9a8b2658ffdeaa9d22c60c1a6ca82424fecdfb12
nidhal-98 <nidhalsalim98@gmail.com>
2023-04-27 10:44:17 +0400

    Followers API

--- Social-Networking-Platform/src/main/java/com/example/SocialNetworkingPlatform/Models/Followers.java
+++ Social-Networking-Platform/src/main/java/com/example/SocialNetworkingPlatform/Models/Followers.java
@@ -1,4 +1,25 @@
 package com.example.SocialNetworkingPlatform.Models;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "followers")
 public class Followers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int follower_id;
+
+    /*
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    Users user;*/
 }
