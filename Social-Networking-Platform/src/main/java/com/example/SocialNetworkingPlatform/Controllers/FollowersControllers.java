e39ada674d98af43df47d95676acec53072fa459
nidhal-98 <nidhalsalim98@gmail.com>
2023-04-27 10:43:47 +0400

    Followers API

--- Social-Networking-Platform/src/main/java/com/example/SocialNetworkingPlatform/Controllers/FollowersControllers.java
+++ Social-Networking-Platform/src/main/java/com/example/SocialNetworkingPlatform/Controllers/FollowersControllers.java
@@ -1,4 +1,24 @@
 package com.example.SocialNetworkingPlatform.Controllers;
 
+import com.example.SocialNetworkingPlatform.Models.Events;
+import com.example.SocialNetworkingPlatform.Models.Followers;
+import com.example.SocialNetworkingPlatform.Services.FollowersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("followers")
 public class FollowersControllers {
+
+    @Autowired
+    FollowersService followersService;
+
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Followers> getAllFollowers() {
+        return followersService.getAllFollowers();
+    }
 }
--- Social-Networking-Platform/src/main/java/com/example/SocialNetworkingPlatform/Services/FollowersService.java
+++ Social-Networking-Platform/src/main/java/com/example/SocialNetworkingPlatform/Services/FollowersService.java
@@ -1,4 +1,21 @@
 package com.example.SocialNetworkingPlatform.Services;
 
+import com.example.SocialNetworkingPlatform.Models.Events;
+import com.example.SocialNetworkingPlatform.Models.Followers;
+import com.example.SocialNetworkingPlatform.Respositories.FollowersRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FollowersService {
+
+    @Autowired
+    FollowersRespository followersRespository;
+
+    public List<Followers> getAllFollowers() {
+        return followersRespository.findAll();
+    }
+
 }
